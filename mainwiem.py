4d2acb4a46e21d4d3c6ece6d2c705632e14f1bd9
Wiem <wiem@charp.co>
2015-12-02 17:43:17 +0100

    adding the test main

--- mainwiem.py
+++ mainwiem.py
@@ -7,7 +7,7 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.cross_validation import train_test_split
 import cleantext as ct
 import loadFiles as lf
-from nltk import word_tokenize
+from nltk import word_tokenize	
 import string
 from nltk.corpus import stopwords
 
@@ -32,14 +32,56 @@ for review in train:
 
 	data2.append(review)
 
-data = data2[:10000]
-labels = labels[:10000]
+data = data2
+labels = labels
 
+from time import time
+t0 = time()
 tfidf_vectorizer = TfidfVectorizer()
 tfidf_matrix = tfidf_vectorizer.fit_transform(data)
 
-tfidf_matrix = tfidf_matrix.toarray()
+#tfidf_matrix = tfidf_matrix.toarray()
 print "size of the matrix", tfidf_matrix.shape
 
 data_train, data_test, labels_train, labels_test = train_test_split(tfidf_matrix, labels, test_size = 0.4, random_state  =42) 
+
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB(alpha = .01)
+y_score = clf.fit(data_train, labels_train)
+labels_predicted= clf.predict(data_test)
+t1=time() -t0
+print "-------------------Vectorizing and fitting the model took %s------------------" %t1
+from sklearn.metrics import classification_report
+print "classification report"
+print classification_report(labels_test, labels_predicted)
+
+
+from sklearn.metrics import accuracy_score
+print "the accuracy score is", accuracy_score(labels_test, labels_predicted)
+
+
+
+"""
+compute the roc curve and the area under curve 
+
+"""
+from sklearn.metrics import roc_curve, auc
+
+fpr, tpr, thresholds = roc_curve(labels_test, y_score[:,1])
+
+roc_auc = auc(fpr, tpr)
+
+print "area under the ROC Curve", roc_auc
+import matplotlib.pyplot as plt
+plt.clf()
+plt.plot(fpr, tpr, label = 'ROC curve')
+plt.plot([0,1], [0,1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('False Positive Rate')
+plt.ylabel( 'True Positive Rate')
+
+plt.title(' Receiver operating characteristic example')
+plt.legend(loc ='lower right')
+plt.show()
 
