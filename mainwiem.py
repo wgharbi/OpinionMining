9af92028e55484279db5082811005cde0749a3a2
Hugo Vallet <hugvallet@gmail.com>
2015-12-05 17:36:54 +0100

    models names added, training on bigrams, better error rates (cohérent avec papier de stanford)

--- mainwiem.py
+++ mainwiem.py
@@ -1,18 +1,25 @@
 import pandas as pd
 import numpy as np 
+
+#preprocessing librairies and functions
+import cleantext as ct
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+#Models librairies
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
 
-
-
+#Other
 from time import time
 
 import matplotlib.pyplot as plt
 
-from sklearn.cross_validation import train_test_split
-import cleantext as ct
+
+
 import loadFiles as lf
 
 
@@ -28,8 +35,8 @@ train = ct.removehtml(data)
 #Create the dictionnary
 data=ct.stemTokenize(train)  
 
-#Compute tf-idf
-tfidf_vectorizer = TfidfVectorizer()
+#Compute tf-idf including n_grams of size 2
+tfidf_vectorizer = TfidfVectorizer(ngram_range=(1,2))
 tfidf_matrix = tfidf_vectorizer.fit_transform(data)
 
 #tfidf_matrix = tfidf_matrix.toarray()
@@ -42,14 +49,16 @@ print "size of the matrix : ", tfidf_matrix.shape
 #define test set and traing set
 data_train, data_test, labels_train, labels_test = train_test_split(tfidf_matrix, labels, test_size = 0.4, random_state  =42)
 #Fix the number of models to train 
-nb_models = 2
-
+nb_models = 3
+model_names=[]
 labels_predicted = np.zeros((len(labels_test),nb_models))
 
 
 #%% Fit first model : Multinomial Naive Bayes Model
 
 from sklearn.naive_bayes import MultinomialNB
+model_names.append("MultinomialNB")
+
 t0 = time()
 clf = MultinomialNB(alpha = .01)
 y_score = clf.fit(data_train, labels_train)
@@ -66,26 +75,31 @@ print "the accuracy score is :", accuracy_score(labels_test, labels_predicted[:,
 #%% Fit second model : SVC
 
 from sklearn.svm import LinearSVC
+model_names.append("LinearSVC")
+
 t1 = time()
-clf2 = LinearSVC(C=1.0)
+clf2 = LinearSVC(C=1)
 y_score2 = clf2.fit(data_train, labels_train)
 labels_predicted[:,1]= clf2.predict(data_test)
 t2=time() -t1
 print "-------------------Vectorizing and fitting the linear SVC took %s"%t2,"sec---------------"
 print "classification report"
 print classification_report(labels_test, labels_predicted[:,1])
+print "the accuracy score is :", accuracy_score(labels_test, labels_predicted[:,1])
 
-#%% Find best parameters for SVC
-from sklearn.grid_search import GridSearchCV
-svc = GridSearchCV( LinearSVC(),  cv = 5, param_grid={"C":np.logspace(-2, 2, 5)})
-svc.fit(data_train, labels_train)
-print "best parameter", svc.best_params_
-labels_predicted= svc.predict(data_test)
+#%% Fit third model : Logistic Regression
+from sklearn.linear_model import LogisticRegression
+model_names.append("Log-Regression")
+
+t1 = time()
+clf3 = LogisticRegression()
+y_score3 = clf3.fit(data_train, labels_train)
+labels_predicted[:,2]= clf3.predict(data_test)
+t2=time() -t1
+print "-------------------Vectorizing and fitting the Log-reg took %s"%t2,"sec---------------"
 print "classification report"
-print classification_report(labels_test, labels_predicted)
-from sklearn.metrics import accuracy_score
-print "the accuracy score is", accuracy_score(labels_test, labels_predicted)
-#the accuracy score is 0.8783 for svc and 0.83 for NB
+print classification_report(labels_test, labels_predicted[:,2])
+print "the accuracy score is :", accuracy_score(labels_test, labels_predicted[:,2])
 
 
 #%%Compute the ROC curve of all models learnt
@@ -105,7 +119,8 @@ for i in range(nb_models) :
 print "area under the ROC Curve", roc_auc
 
 for i in range(nb_models) :
-    plt.plot(fpr[i], tpr[i], label = 'ROC curve model %s'%i)
+    name=model_names[i]
+    plt.plot(fpr[i], tpr[i], label = name)
     
 plt.plot([0,1], [0,1], 'k--')
 plt.xlim([0.0, 1.0])
@@ -116,3 +131,15 @@ plt.ylabel( 'True Positive Rate')
 plt.title('Models ROC curve')
 plt.legend(loc ='lower right')
 plt.show()
+
+#%% Find best parameters for SVC
+from sklearn.grid_search import GridSearchCV
+svc = GridSearchCV( LinearSVC(),  cv = 5, param_grid={"C":np.logspace(-2, 2, 5)})
+svc.fit(data_train, labels_train)
+print "best parameter", svc.best_params_
+labels_predicted= svc.predict(data_test)
+print "classification report"
+print classification_report(labels_test, labels_predicted)
+from sklearn.metrics import accuracy_score
+print "the accuracy score is", accuracy_score(labels_test, labels_predicted)
+#the accuracy score is 0.8783 for svc and 0.83 for NB
